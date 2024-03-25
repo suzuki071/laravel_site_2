4c4a524f5d4781642bb7f5e075773989a8652998
suzuki071 <83170397+suzuki071@users.noreply.github.com>
2024-03-25 20:36:31 +0900

    parent layout add

---
+++ resources/views/parentlayout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @yield('head')
+  @yield('title')
+</head>
+<body>
+  @yield(content)
+</body>
+</html>
